bf44d3035aa69093a377ae492a72df8c33bd6e85
Luciano Filho <lvgalvaofilho@gmail.com>
2022-12-03 05:25:45 -0300

    add exemplo_01

---
+++ exemplo_01.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Olá internet ou 3nao!!!")
+
+st.markdown("""
+# Olá esse é um titulo em markdown
+** negrito ** , *italico*
+
+##Subtitulo
+""")
+
+st.image("https://via.placeholder.com/500x450")
